3583c3bcd8c9a5a1c8042d2f79cc3e9f9978fcdb
unknown <ad971718@ohio.edu>
2022-01-14 18:26:19 -0500

    Added comments

--- filesize.cc
+++ filesize.cc
@@ -8,20 +8,23 @@ int main( int argc, char* argv[] )
 {
     string temp;
     cout << "program: " << argv[0] << '\n';
+
     for (int arg = 1; arg < argc; ++arg)
     {
-            int n = 0;
+            int n = 0;  
             cout << " " << argv[arg] << ": ";
-            ifstream file(argv[arg]);
-            if (!file)
+            ifstream file(argv[arg]);   // Open the file found at the current index of argument vector
+
+            if (!file)  // If file doesnt exist num lines is -1
             {
                 n = -1;
             }   
-            while(getline(file, temp))
+            while(getline(file, temp))  // If a line is present increment n
             {
                 n++;
             }
             cout << n << '\n';
     }
-    exit(0); // this means that the program executed correctly!
+
+    exit(0); 
 }
